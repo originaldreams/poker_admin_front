ed57d7b8e3a322ac95d4ffd06dbc02f7807bc203
goetboy <32829163+goetboy@users.noreply.github.com>
2018-08-19 22:48:41 +0800

    全局请求处理

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,82 +1,89 @@
 "use strict";
-import router from 'vue-router'
+
 import Vue from 'vue';
 import axios from "axios";
-import {
-  deleteCookie
-} from "../utils/cookie"
-
+import {Message} from "element-ui";
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
-  // baseURL: process.env.baseURL || process.env.apiUrl || ""
-  // timeout: 60 * 1000, // Timeout
-  // withCredentials: true, // Check cross-site Access-Control
+    // baseURL: process.env.baseURL || process.env.apiUrl || ""
+    // timeout: 60 * 1000, // Timeout
+    // withCredentials: true, // Check cross-site Access-Control
 };
 
 const _axios = axios.create(config);
 
 _axios.interceptors.request.use(
-  function (config) {
-    // Do something before request is sent
-    return config;
-  },
-  function (error) {
-
-    return Promise.reject(error);
-  }
-);
-axios.interceptors.response.use(
-
-  response => {
-    return response;
-  },
-  error => {
-    if (error.response) {
-      switch (error.response.status) {
-        case 401:
-          // 返回 401 清除token信息并跳转到登录页面
-          // store.commit(types.LOGOUT);
-          // 登出
-          // axios.get(auth_url.logout)
-          deleteCookie("sessionid");
-          router.push('/login');
-      }
+    function (config) {
+        // Do something before request is sent
+        return config;
+    },
+    function (error) {
+        // Do something with request error
+        return Promise.reject(error);
     }
-    return Promise.reject(error.response.data) // 返回接口返回的错误信息
-  });
+);
+
 // Add a response interceptor
 _axios.interceptors.response.use(
-  function (response) {
-    // Do something with response data
-    return response;
-  },
-  function (error) {
-    // Do something with response error
-    if (error.state == 401)
-      droute.push('/login');
-    return Promise.reject(error);
-  }
+    function (response) {
+        console.log(response)
+        if (response.status == 200) {
+            let data = response.data;
+            if (data.success == 0)
+                return data.data;
+        }
+
+    },
+    function (error) {
+        if (error.response) {
+            let response = error.response
+            switch (response.status) {
+                case 500:
+                    Message({
+                        showClose: true,
+                        message: '请求异常,请重试',
+                        type: "error"
+                    })
+                    break;
+                case 403:
+                    Message({
+                        showClose: true,
+                        message: response.data.message
+                        , type: 'error'
+                    })
+                    break;
+                default:
+                    Message({
+                        showClose: true,
+                        message: '未知错误'
+                        , type: 'error'
+                    })
+            }
+
+        }
+        return Promise.reject(error);
+    }
 );
 
 Plugin.install = function (Vue, options) {
-  Vue.axios = _axios;
-  window.axios = _axios;
-  Object.defineProperties(Vue.prototype, {
-    axios: {
-      get() {
-        return _axios;
-      }
-    },
-    $axios: {
-      get() {
-        return _axios;
-      }
-    },
-  });
+    Vue.axios = _axios;
+    window.axios = _axios;
+    Object.defineProperties(Vue.prototype, {
+        axios: {
+            get() {
+                return _axios;
+            }
+        },
+        $axios: {
+            get() {
+                return _axios;
+            }
+        },
+    });
 };
 
 Vue.use(Plugin)
